7183ed6dad0e2017047d997d6929533392b7fe2d
Yousuf <124455652+yousufSadeqi@users.noreply.github.com>
2025-03-29 15:30:14 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -34,7 +34,7 @@ app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
 /* SERVER */
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 80;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on part ${port}`);
 });
